6444fc2752f0d6a3f07e1985af242b7c7133e5f6
Aksu <appsu-@hotmail.com>
2018-12-08 13:04:54 +0200

    Adding the project

---
+++ src/main/java/fi/projekti/LinkList/LinkListApplication.java
@@ -0,0 +1,12 @@
+package fi.projekti.LinkList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkListApplication.class, args);
+	}
+}
---
+++ src/test/java/fi/projekti/LinkList/LinkListApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.projekti.LinkList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinkListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
